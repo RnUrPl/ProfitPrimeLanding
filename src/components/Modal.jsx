3d49a130cf2c41052622f09baf43d9229c41e530
RnUrPl <116657800+RnUrPl@users.noreply.github.com>
2024-01-13 16:40:16 +0100

    fixed

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -10,18 +10,19 @@ const Modal = ({ trader, closeModal }) => {
           Close
         </button>
         <h2>{trader.name}</h2>
-        <Suspense fallback={<CanvasLoader />}>
-        <div className="trader_content">
+        
+   
+{   trader.modal &&    (  <div className="trader_content">
             <img src={trader.modal}className='trader_modal_img'/>
         </div>
-        </Suspense>
+      )}
         
         {trader.video && (
-                <Suspense fallback={<CanvasLoader />}>
+
           <div className="trader_content">
             <video src={trader.video} className='trader_modal_img' controls autoPlay />
           </div>
-          </Suspense>
+ 
         )}
       </div>
     </div>
